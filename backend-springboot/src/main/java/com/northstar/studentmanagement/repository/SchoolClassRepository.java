5b4d76e20ef21ecd592604e7c4ecdb6e6c487004
psbalaji57-star <psbalaji57@gmail.com>
2026-09-14 00:10:24 -0700

    Delete backend-springboot directory
    removing dir

--- backend-springboot/src/main/java/com/northstar/studentmanagement/repository/SchoolClassRepository.java
+++
@@ -1,6 +0,0 @@
-package com.northstar.studentmanagement.repository;
-
-import com.northstar.studentmanagement.entity.SchoolClass;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SchoolClassRepository extends JpaRepository<SchoolClass, Long> {}
